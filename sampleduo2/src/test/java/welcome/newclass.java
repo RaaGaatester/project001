5c39dc30e092b4bf06a7d0f7c8714b4a9bfe5f7b
Adithya Ravi <aadhy123@gmail.com>
2023-05-22 10:20:02 +0530

    This is the comitt changes done

---
+++ sampleduo2/src/test/java/welcome/newclass.java
@@ -0,0 +1,12 @@
+package welcome;
+
+public class newclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Name is :Adithya");
+
+	}
+
+}
